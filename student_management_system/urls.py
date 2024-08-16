c8ea0fafc6cb67090171d684d9f12a6e87d3af15
Aster07102000 <aster.r@codezen.in>
2024-08-16 09:36:13 +0530

    16-08-24

--- student_management_system/urls.py
+++ student_management_system/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('students.urls')),
+    path('', include('home.urls')),
+    path('students/', include('students.urls')),
 ]
