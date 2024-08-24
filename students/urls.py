2f935ec23a53dba212ea21ba65b547dea8cd7857
Aster07102000 <aster.r@codezen.in>
2024-08-24 10:07:43 +0530

    fist ocmmit

--- students/urls.py
+++ students/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.student_list, name='student-list'),
-    path('add-student', views.add_student, name='add-student'),
+    path('add-student/', views.add_student, name='add-student'),
+    path('post-student/', views.postStudent, name='post-student'),
+
 ]
--- students/views.py
+++ students/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import Students
+from .forms import StudentForm
 
 # Create your views here.
 def student_list(request):
@@ -8,3 +10,19 @@ def student_list(request):
 
 def add_student(request):
     return render(request, 'students/add_student.html')
+
+def postStudent(request):
+    if request.method == 'POST':
+        form = StudentForm(request.POST)
+        try:
+            form.save()
+            messages.success(request, 'Student added successfully!')
+            return redirect('student-list')
+        except Exception as e:
+                messages.error(request, f'Error: {str(e)}')
+        else:
+            messages.error(request, 'Please correct the errors below.')
+    else: 
+        form = StudentForm()
+    
+    return render(request, 'students/add_student.html', {'form': form})
