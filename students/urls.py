12a1ba7617adbd70730290db42b1c1ba135d7b31
Aster07102000 <aster.r@codezen.in>
2024-09-07 09:10:29 +0530

    fsit commit\

--- students/urls.py
+++ students/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.student_list, name='student-list'),
     path('add-student/', views.add_student, name='add-student'),
+    path('edit-student/<str: strudent_id>', views.add_student, name='add-student'),
     path('post-student/', views.postStudent, name='post-student'),
-
+    path('post-student/<str: strudent_id>', views.putStudent, name='put-student'),
 ]
--- students/views.py
+++ students/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 from .models import Students
 from .forms import StudentForm
@@ -25,5 +25,23 @@ def postStudent(request):
             messages.error(request, form.errors)
     else: 
         form = StudentForm()
+    
+    return render(request, 'students/add_student.html', {'form': form})
+
+def putStudent(request):
+    student = get_object_or_404(Students, student_id = student_id)
+    if request.method == 'POST':
+        form = StudentForm(request.POST, instance=student)
+        if form.is_valid():
+            try:
+                form.save()
+                messages.success(request, 'Student updated successfully!')
+                return redirect('student-list')
+            except Exception as e:
+                messages.error(request, f'Error: {str(e)}')
+        else:
+            messages.error(request, form.errors)
+    else: 
+        form = StudentForm(instance=student)
     
     return render(request, 'students/add_student.html', {'form': form})
